5be3c4ce18a73ca591ebfdb5badc1de5cd16683e
elmartipro <duartemartin62@gmail.com>
2025-08-12 10:12:49 -0500

    a

--- Assets/Dice/audio_stream_player.gd
+++ Assets/Dice/audio_stream_player.gd
@@ -1,4 +1,1 @@
 extends AudioStreamPlayer
-
-var start_time = 0.32 # segundos desde donde quieres empezar
-var duration = 0.20    # duración del fragmento
--- levels/control.gd
+++ levels/control.gd
@@ -6,16 +6,21 @@ extends Control
 @onready var boton_cerrar = $CanvasLayer/VentanaEmergente/CerrarVentana
 
 func _ready():
-	ventana.visible = false
-	boton_mostrar.pressed.connect(_on_mostrar_ventana)
-	boton_cerrar.pressed.connect(_on_cerrar_ventana)
+	if ventana:
+		ventana.visible = false
+		boton_mostrar.pressed.connect(_on_mostrar_ventana)
+		boton_cerrar.pressed.connect(_on_cerrar_ventana)
+	else:
+		print("No se encontro el nodo")
 
 func _on_mostrar_ventana():
 	texto_ventana.text = "🩺 ¡Bienvenido a Ludoclínica!"
-	ventana.popup_centered()  # Muestra la ventana centrada
+	ventana.visible = true
+	ventana.set_position((get_viewport_rect().size - ventana.size) / 2) 
+
 
 func _on_cerrar_ventana():
-	ventana.hide()
+	ventana.visible = false
 
 
 func _on_button_pressed() -> void:
