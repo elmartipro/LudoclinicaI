2e53eeaf526e8a1c5ef773533e809079c91e2983
elmartipro <duartemartin62@gmail.com>
2025-08-22 11:54:54 -0500

    a

--- Assets/Podium/podium.gd
+++ Assets/Podium/podium.gd
@@ -1,15 +1,21 @@
 extends Node3D
 
 @export var assets : Array[PackedScene] = [
-	preload("")
+	preload("res://Assets/Podium/pA.tscn"),
+	preload("res://Assets/Podium/pB.tscn"),
+	preload("res://Assets/Podium/pC.tscn"),
+	preload("res://Assets/Podium/pD.tscn"),
+	preload("res://Assets/Podium/pE.tscn"),
+	preload("res://Assets/Podium/pF.tscn"),
+	preload("res://Assets/Podium/pG.tscn"),
+	preload("res://Assets/Podium/pH.tscn")
 ]
 
+var podiums : Array[PackedScene] = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if assets.is_empty():
+		return
+	var random_index = randi() % assets.size()
+	var instance = assets[random_index].instantiate()
+	add_child(instance)
