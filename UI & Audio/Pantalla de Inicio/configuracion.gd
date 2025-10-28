3000a1d59eb98241429205b7ff613a5fc141ad97
elmartipro <35909943+elmartipro@users.noreply.github.com>
2025-10-28 17:41:06 -0500

    Add persistent title video and data reset option

--- UI & Audio/Pantalla de Inicio/configuracion.gd
+++ UI & Audio/Pantalla de Inicio/configuracion.gd
@@ -1,7 +1,26 @@
 extends Control
 
 
+func _ready() -> void:
+	TitleBackground.ensure_visible()
 
 
 func _on_volver_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI & Audio/Pantalla de Inicio/pantalla_de_incio.tscn")
+
+
+func _on_borrar_datos_pressed() -> void:
+	var deleted := false
+	for path in ["user://exam_results.json", "user://config.cfg"]:
+		if FileAccess.file_exists(path):
+			var error := DirAccess.remove_absolute(path)
+			if error == OK:
+				deleted = true
+	if deleted:
+		OS.alert("Los datos guardados se eliminaron correctamente.", "Datos reiniciados")
+	else:
+		OS.alert("No se encontraron datos guardados para eliminar.", "Datos reiniciados")
+
+
+func _on_music_control_value_changed(_value: float) -> void:
+	pass
--- UI & Audio/Pantalla de Inicio/pantalla_de_incio.gd
+++ UI & Audio/Pantalla de Inicio/pantalla_de_incio.gd
@@ -1,10 +1,10 @@
 extends Control
 
-# Musica De el juego 
-var sonido_fondo := preload("res://UI & Audio/Musica y Audios/main_music.mp3") 
-
+# Musica De el juego
+var sonido_fondo := preload("res://UI & Audio/Musica y Audios/main_music.mp3")
 
 func _ready():
+	TitleBackground.ensure_visible()
 	# Make sure we assign the right stream
 	if MusicaDeFondo.stream != sonido_fondo:
 		MusicaDeFondo.stream = sonido_fondo
@@ -12,15 +12,15 @@ func _ready():
 	# Ensure it loops forever
 	if MusicaDeFondo.stream is AudioStream:
 		MusicaDeFondo.stream.set_loop(true)  # Godot 4.x
-	
+
 	# Play if not already playing
 	if not MusicaDeFondo.playing:
 		MusicaDeFondo.play()
 
 
-
-# Jugar redirecciona a la escena de escoger modos 
-func _on_play_pressed(): 
+# Jugar redirecciona a la escena de escoger modos
+func _on_play_pressed():
+	TitleBackground.hide_background()
 	get_tree().change_scene_to_file("res://UI & Audio/Pantalla de Inicio/modos_de_juego.tscn")
 
 
@@ -29,4 +29,5 @@ func _on_configuracion_pressed():
 
 
 func _on_salir_pressed():
+	TitleBackground.hide_background()
 	get_tree().quit()
