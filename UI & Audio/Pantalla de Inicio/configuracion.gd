2b8bf5c036364492790aaf4fc7bad24785822860
elmartipro <duartemartin62@gmail.com>
2025-10-28 18:26:14 -0500

    a

--- UI & Audio/Pantalla de Inicio/configuracion.gd
+++ UI & Audio/Pantalla de Inicio/configuracion.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_volver_2_pressed() -> void:
-      get_tree().change_scene_to_file("res://UI & Audio/Pantalla de Inicio/pantalla_de_incio.tscn")
+	get_tree().change_scene_to_file("res://UI & Audio/Pantalla de Inicio/pantalla_de_incio.tscn")
 
 
 func _on_borrar_datos_pressed() -> void:
--- UI & Audio/Pantalla de Inicio/pantalla_de_incio.gd
+++ UI & Audio/Pantalla de Inicio/pantalla_de_incio.gd
@@ -4,9 +4,9 @@ extends Control
 var sonido_fondo := preload("res://UI & Audio/Musica y Audios/main_music.mp3")
 
 func _ready():
-      # Make sure we assign the right stream
-      if MusicaDeFondo.stream != sonido_fondo:
-              MusicaDeFondo.stream = sonido_fondo
+	# Make sure we assign the right stream
+	if MusicaDeFondo.stream != sonido_fondo:
+		MusicaDeFondo.stream = sonido_fondo
 
 	# Ensure it loops forever
 	if MusicaDeFondo.stream is AudioStream:
@@ -19,7 +19,7 @@ func _ready():
 
 # Jugar redirecciona a la escena de escoger modos
 func _on_play_pressed():
-      get_tree().change_scene_to_file("res://UI & Audio/Pantalla de Inicio/modos_de_juego.tscn")
+	get_tree().change_scene_to_file("res://UI & Audio/Pantalla de Inicio/modos_de_juego.tscn")
 
 
 func _on_configuracion_pressed():
@@ -27,4 +27,4 @@ func _on_configuracion_pressed():
 
 
 func _on_salir_pressed():
-      get_tree().quit()
+	get_tree().quit()
